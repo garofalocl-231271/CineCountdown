e578def6e31ece043bbf3b36aa113a76e5b1842f
garofalocl-231271 <garofalo.cl@gmail.com>
2026-02-13 00:59:09 +0100

    Add files via upload

--- test/normalizer/cinema.normalizer.js
+++ test/normalizer/cinema.normalizer.js
@@ -1,1 +1,28 @@
+window.CinemaNormalizer = (function () {
+  function buildMaps(source) {
+    const MULTIPLEX_MAP = {};
+    const CITY_MAP = {};
 
+    Object.values(source.multiplex).forEach(m => {
+      MULTIPLEX_MAP[m.label] = m.id;
+    });
+
+    Object.values(source.cities).forEach(c => {
+      const multiplexLabel =
+        source.multiplex[c.multiplexId]?.label;
+
+      if (!multiplexLabel) return;
+
+      const uiLabel = `${multiplexLabel} • ${c.label}`;
+
+      CITY_MAP[uiLabel] = {
+        id: c.id,
+        multiplex: c.multiplexId
+      };
+    });
+
+    return { MULTIPLEX_MAP, CITY_MAP };
+  }
+
+  return { buildMaps };
+})();
